95d4d0e04f1c8044b1e7d63c1b1b32240c7725d8
Andrii <a.nahornyi@be-tse.com>
2025-02-11 13:39:29 +0100

    Revert "Added classes"
    This reverts commit bbe433f5458c755f141edcfade9abe5cb10375b3.

--- src/main/java/com/example/usermanagement/controllers/PaymentsController.java
+++
@@ -1,4 +0,0 @@
-package com.example.usermanagement.controllers;
-
-public class PaymentsController {
-}
--- src/main/java/com/example/usermanagement/services/PaymentService.java
+++
@@ -1,4 +0,0 @@
-package com.example.usermanagement.services;
-
-public class PaymentService {
-}
