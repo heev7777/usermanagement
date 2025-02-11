d29de71fc4b3a26da670b358bf24beeaa6715a47
enesfarukbaltaci <jesus.hernandez@imagina-bpo.es>
2025-02-11 12:59:56 +0000

    Controller and Service class created

--- src/main/java/com/example/usermanagement/repositories/PaymentRepository.java
+++ src/main/java/com/example/usermanagement/repositories/PaymentRepository.java
@@ -3,5 +3,8 @@ package com.example.usermanagement.repositories;
 import com.example.usermanagement.models.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findByReservationId(Long reservationId);
 }
--- src/main/java/com/example/usermanagement/services/PaymentService.java
+++ src/main/java/com/example/usermanagement/services/PaymentService.java
@@ -1,4 +1,32 @@
 package com.example.usermanagement.services;
 
+import com.example.usermanagement.models.Payment;
+import com.example.usermanagement.repositories.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PaymentService {
+
+    @Autowired
+    private PaymentRepository repository;
+
+    public Payment registerPayment(Long reservationId, Double amount) {
+        Payment payment = new Payment(reservationId, amount, true);
+        return repository.save(payment);
+    }
+
+    public List<Payment> listAllPayments() {
+        return repository.findAll();
+    }
+
+    public boolean checkPaymentStatus(Long reservationId) {
+        return repository.findByReservationId(reservationId).stream().anyMatch(Payment::isPaid);
+    }
+
+    public Double calculateTotalAmount(Long reservationId) {
+        return repository.findByReservationId(reservationId).stream().mapToDouble(Payment::getAmount).sum();
+    }
 }
