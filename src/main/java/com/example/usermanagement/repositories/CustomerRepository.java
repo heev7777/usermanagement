cf0cbb0a4b06e84e4abfcfa41296049851bbc205
Maist <maistsi@gmail.com>
2025-02-12 10:47:18 +0100

    feat: implement CustomerRepository

--- src/main/java/com/example/usermanagement/repositories/CustomerRepository.java
+++ src/main/java/com/example/usermanagement/repositories/CustomerRepository.java
@@ -1,6 +1,6 @@
 package com.example.usermanagement.repositories;
 
+import com.example.usermanagement.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<User, Long> {
-}
+public interface CustomerRepository extends JpaRepository<Customer, Long> {}
