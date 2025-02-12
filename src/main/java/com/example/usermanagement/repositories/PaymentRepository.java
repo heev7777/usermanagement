56ddf4d296d14d98d1302bbe2f2c321ca8e324ed
Andrii <a.nahornyi@be-tse.com>
2025-02-12 10:38:30 +0100

    Changes methods in Services

--- src/main/java/com/example/usermanagement/repositories/PaymentRepository.java
+++ src/main/java/com/example/usermanagement/repositories/PaymentRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    List<Payment> findByReservationId(Long reservationId);
 }
